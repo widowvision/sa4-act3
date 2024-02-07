39e978ffd59afccf4172cc99045dd52d6299b3eb
Libbyclaire McGhee <lcmcghe1@memphis.edu>
2024-02-07 11:15:45 -0600

    Gives hints for users

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,23 @@
 number = 10
 guess = 0
+val = 'a'
 
 print("I'm thinking of a number...\nPress 'q' to quit")
-while(guess != 'q'):
-    guess = input("What number am I thinking of? ")
+while(val != 'q'):
+    guess = int(input("What number am I thinking of? "))
 
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif guess < number:
+        print("Too low!")
+        val = input("Press q to quit or any key to continue ")
+    elif guess > number:
+        print("Too high!")
+        val = input("Press q to quit or any key to continue ")
     else: 
-        print("Wrong guess!")
+        print("Wrong guess! Press q to quit, or any key to continue ")
+        val = input()
       
 
 print(f"Sorry! The number was {number}")
