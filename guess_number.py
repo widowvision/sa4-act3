0f1c07fe0307536028a9f29f3376cbb2417ba53d
Libbyclaire McGhee <lcmcghe1@memphis.edu>
2024-02-07 11:03:37 -0600

    Updated code to allow multiple attempts, and let user quit

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,15 @@
 number = 10
+guess = 0
 
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of?"))
+print("I'm thinking of a number...\nPress 'q' to quit")
+while(guess != 'q'):
+    guess = input("What number am I thinking of? ")
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else: 
-    print(f"Sorry! The number was {number}")
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else: 
+        print("Wrong guess!")
+      
+
+print(f"Sorry! The number was {number}")
